47b4e9b446cdc5baed9bff24993a0feb1562079d
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-16 00:28:16 +0500

    add redirect from `/login` for authorized users

--- src/screens/LoginScreen/Components/LoginScreen.tsx
+++ src/screens/LoginScreen/Components/LoginScreen.tsx
@@ -1,5 +1,13 @@
-import { LoginForm } from '../../../features/auth';
+import { Navigate } from 'react-router-dom';
 
-const LoginScreen: React.FC = () => <LoginForm />;
+import { LoginForm } from '../../../features/auth';
+import { useAuth } from '../../../features/auth/Hooks';
+import { routingService } from '../../../infrastructure/RoutingService';
+
+const LoginScreen: React.FC = () => {
+  const { loggedIn } = useAuth();
+
+  return loggedIn ? <Navigate to={routingService.kitty()} /> : <LoginForm />;
+};
 
 export default LoginScreen;
