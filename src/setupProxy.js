18401fd72f8915cc377dc168b939ca87fd387c09
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-16 00:08:10 +0500

    fix: bug with localhost

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const { API_URL } = require('./utils/constants');
 
 module.exports = function(app) {
   app.use(
-    API_URL,
+    '/api/v1',
     createProxyMiddleware({
       target: 'http://51.250.65.73',
       changeOrigin: true,
