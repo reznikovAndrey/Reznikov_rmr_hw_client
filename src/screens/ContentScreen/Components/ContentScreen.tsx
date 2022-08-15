c672c2f1337321f87f4591348c82857260324019
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-15 14:10:37 +0500

    fix: make kitty image responsive

--- src/screens/ContentScreen/Components/ContentScreen.tsx
+++ src/screens/ContentScreen/Components/ContentScreen.tsx
@@ -1,9 +1,11 @@
+import styles from './ContentScreen.module.scss';
+
 import { useAuth } from '../../../features/auth/Hooks';
 
 const ContentScreen: React.FC = () => {
   const { imgSrc } = useAuth();
 
-  return imgSrc ? <img src={imgSrc} alt="kitty" /> : null;
+  return imgSrc ? <img src={imgSrc} alt="kitty" className={styles.kitty} /> : null;
 };
 
 export default ContentScreen;
