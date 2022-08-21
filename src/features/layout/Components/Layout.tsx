fc57810af04ccbba25b70e9e786944a5e911bfb1
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-21 15:42:08 +0500

    fix: remove unused import

--- src/features/layout/Components/Layout.tsx
+++ src/features/layout/Components/Layout.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from 'axios';
-import { useCallback, useEffect, useMemo, useState, memo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
